7a98d7a444284b97a79a70712cb796248b2d7a50
Kevin Chik <kevin.c.chik@gmail.com>
2019-05-23 15:00:48 -0400

    Hack

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App.jsx'
 
+setInterval(() => {
+  fetch('https://limelight-dance-ui.herokuapp.com/')
+  fetch('https://limelight-dance-core.herokuapp.com/')
+}, 1000)
+
 ReactDOM.render(<App />, document.getElementById('root'))
