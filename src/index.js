c466c278ce7bfa46cdc72de5f625ca97c0797acb
Kevin Chik <kevin.c.chik@gmail.com>
2019-05-23 15:03:57 -0400

    YAAA

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App.jsx'
 
+// Prevents heroku apps from sleeping by pinging them every 5 minutes
 setInterval(() => {
   fetch('https://limelight-dance-ui.herokuapp.com/')
   fetch('https://limelight-dance-core.herokuapp.com/')
-}, 1000)
+}, 300000)
 
 ReactDOM.render(<App />, document.getElementById('root'))
