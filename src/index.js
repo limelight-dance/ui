70686f0c14be45e878e5e76e566b8f2bf206a223
Kevin Chik <kevin.c.chik@gmail.com>
2019-05-27 13:17:46 -0400

    Remove ping hack

--- src/index.js
+++ src/index.js
@@ -2,12 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App.jsx'
 
-// Prevents heroku apps from sleeping by pinging them every 5 minutes
-if (process.env.PRODUCTION) {
-  setInterval(() => {
-    fetch(process.env.URL)
-    fetch(process.env.CORE_URL)
-  }, 300000)
-}
-
 ReactDOM.render(<App />, document.getElementById('root'))
