caa937a1ad0ef6246e88383e30d6eea80e30553c
Kevin Chik <kevin.c.chik@gmail.com>
2019-07-10 16:26:15 -0400

    lazy commit

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: process.env.PORT,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
